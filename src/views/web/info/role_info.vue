f2e87dd9c1c21a45fbf8f5ca908a8b58e9a4761f
lenovo <sf.lucky667@gmail.com>
2024-12-16 00:10:36 +0800

    feat:用户权限设置

--- src/views/web/info/role_info.vue
+++ src/views/web/info/role_info.vue
@@ -1,7 +1,8 @@
 <script lang="ts" setup>
-import { IVerificationQuestionType } from '@/api/user_api';
+import {IUserProfileUpdateType, IVerificationQuestionType} from '@/api/user_api';
 import { useUserStore } from '@/stores';
 import { reactive } from 'vue';
+
 
 const store = useUserStore()
 // 0 不允许别人查找到我， 1  通过用户号找到我 2 可以通过昵称搜索到我
@@ -18,7 +19,30 @@ const verificationQuestion = reactive<IVerificationQuestionType>({
 
 </script>
 <template>
-	<div>权限设置</div>
+  <div class="role_info_view">
+    <el-form-item label="防骚扰:">
+      <div>
+        <div>
+      <div class="label">请选择允许别人查找到你的方式：</div>
+          <el-radio-group v-model="userProfile.searchUser">
+            <el-radio :value="0">不允许别人查找到我</el-radio>
+            <el-radio :value="1">只能通过用户号查找到我</el-radio>
+            <el-radio :value="2">可以通过昵称搜索到我</el-radio>
+          </el-radio-group>
+      </div>
+      <div>
+        <div class="label">请选择适合你的验证方式：</div>
+        <el-radio-group v-model="userProfile.verification" >
+          <el-radio :value="0">不允许任何人添加</el-radio>
+          <el-radio :value="1">允许任何人添加</el-radio>
+          <el-radio :value="2">需要验证消息</el-radio>
+          <el-radio :value="3">需要回答问题</el-radio>
+          <el-radio :value="4">需要正确回答问题</el-radio>
+        </el-radio-group>
+      </div>
+      </div>
+    </el-form-item>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
